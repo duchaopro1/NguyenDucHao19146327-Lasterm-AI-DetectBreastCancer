0a4a3855d815636bc619e6f9d07fb9d9ec5a690b
Aoh <105488378+duchaopro1@users.noreply.github.com>
2022-06-21 14:17:53 +0700

    Update web.py

--- web.py
+++ web.py
@@ -7,7 +7,7 @@ from tensorflow.keras.preprocessing.image import img_to_array, load_img
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
 
-model = tf.keras.models.load_model("final_cifar10_CNN_model.h5") #model m train
+model = tf.keras.models.load_model("Hue.h5") #model m train
 
 ### load file
 uploaded_file = st.file_uploader("Choose an image file", type=["jpg","jpeg","png"])
@@ -19,10 +19,10 @@ map_dict = {0: 'BENIGN',
  
 if uploaded_file is not None:
     # Convert the file
-    img = image.load_img(uploaded_file,target_size=(32,32)) #xử lí ảnh theo cách m làm
+    img = image.load_img(uploaded_file,target_size=(64,64)) #xử lí ảnh theo cách m làm
     st.image(uploaded_file, channels="RGB") #hiển thị ảnh
     img = img_to_array(img)
-    img = img.reshape(1,32,32,3)
+    img = img.reshape(1,64,64,3)
     img = img.astype('float32')
     img = img/255
         
